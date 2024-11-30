85d3837c487c4c3daf2edff9f7231e8e60240623
Alla D. Barkat <a2la1101@gmail.com>
2024-11-30 13:58:34 +0200

    The Test
    Generating All possible number of nodes per layer in a way that the total number of nodes matches the required number along with the maximum number of layers.

--- PathPlanning/TestingAlgorithms.py
+++ PathPlanning/TestingAlgorithms.py
@@ -3,10 +3,8 @@
 # 1 1 1 1 : Four layers, each with one 
 # 1 1 2   : 3 layers
 # 1 3     : 2 Layers
-# 2 2     : 2 Layers == Best Solution ==
+# 2 2     : 2 Layers == Best Solution ... maybe lol ==
 # 4       : 1 Layer
-
-from itertools import combinations
 
 def combineStrings(a,b):
     if len(b):
@@ -17,7 +15,7 @@ def combineStrings(a,b):
         for y in b:
             solution.append(f"{x},{y}")
     return solution
-#returns [(x,y,z)....] 
+
 def combination_alla(n, max_layer):
     if max_layer == 0:
         return []
@@ -25,14 +23,15 @@ def combination_alla(n, max_layer):
     for i in range(1,n+1):
         probset=[f"{i}"]
         prev=combination_alla(n-i,max_layer-1)
-        if len(prev) == 0 and i != n: continue
+        if len(prev) == 0 and i != n: continue  
+        # If Max Layer was Reached, then ProbSet won't get us to the required amount of layers, so we move to the next i.
         combs = combineStrings(probset,prev)
         for sol in combs:
             solutions.append( sol )
     return solutions
 
-solveit=combination_alla(40,max_layer=4)
+solveit=combination_alla(26,max_layer=8)
 for i in solveit:
     for item in i:
-        print(f"{item}",end=" ")
+        print(item,end=" ")
     print()
